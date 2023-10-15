ab8420c86954cb4fa72822b797f0b30eec0cfab7
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-10-15 20:28:01 -0300

    Exercício 003

---
+++ CursoEmVideo/Mundo 1/Exercicios/ex003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite outro inteiro: '))
+soma = numero1 + numero2
+
+print('A soma entre {0} e {1} é {2}.'.format(numero1, numero2, soma))
