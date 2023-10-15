671c7860a2d28be8077962f1abbd2685241587b2
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-10-15 16:39:14 -0300

    Iniciando Curso em Vídeo

---
+++ CursoEmVideo/Mundo 1/desafio03.py
@@ -0,0 +1,8 @@
+print('----- DESAFIO 03 -----')
+
+primeiroNumero = int(input('Primeiro número inteiro = '))
+segundoNumero = int(input('Segundo número inteiro = '))
+
+soma = primeiroNumero + segundoNumero
+
+print('A soma é', soma, '.')
